92d59c742dcc8e4c954f7152fb0dc3c986e8e265
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-09 16:36:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,40 @@
 # Write your code here!
+def game_hash
+##
+game_hash = {
+  :home => {
+    
+  }
+  :away => {
+    
+  }
+}
+end
 
+def num_points_scored
+##
+end
 
+def shoe_size
+##
+end
 
+def team_colors
+##
+end
 
+def team_names
+##
+end
 
+def player_numbers
+##
+end
 
+def player_stats
+##
+end
 
-
-
-
+def big_shoe_rebounds
+##
+end
